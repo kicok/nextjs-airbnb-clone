386f0fc4cef637f7094d2b470bab6ecb4f451e95
kicok <kicoker@gmail.com>
2024-02-14 16:41:11 +0900

    현재버전에서는 warning이 뜨지 않으므로 별 의미는 없음. getListing > listings.map 반환, Listing override

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -7,7 +7,13 @@ export default async function getListings() {
             },
         });
 
-        return listings;
+        const safeListings = listings.map((listing) => ({
+            ...listing,
+            createdAt: listing.createdAt.toISOString(),
+        }));
+
+        return safeListings;
+        // return listings; // 현재 버전에서는 이 것을 그대로 사용해도 [warning:Warning: Only plain objects can be passed to Client Components from Server Components] 이 뜨지 않는다.
     } catch (error: any) {
         throw new Error(error);
     }
--- app/type/index.ts
+++ app/type/index.ts
@@ -1,7 +1,12 @@
-import { User } from '@prisma/client';
+import { Listing, User } from '@prisma/client';
+
+// createdAt 프러퍼티를 제거한뒤 string 타입의 createdAt 을 추가
+export type SafeListing = Omit<Listing, 'createdAt'> & {
+    createdAt: string;
+};
 
 export type SafeUser = Omit<User, 'createdAt' | 'updatedAt' | 'emailVerified'> & {
-   createdAt: string;
-   updatedAt: string;
-   emailVerified: string | null;
+    createdAt: string;
+    updatedAt: string;
+    emailVerified: string | null;
 };
